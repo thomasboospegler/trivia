6fb83c866ebe8287b2f3b3d9a05a1dd0b4fabdf9
Thomas Boos Pegler <tboospegler@gmail.com>
2022-09-08 14:14:40 -0400

    Criando redux

---
+++ src/redux/actions/index.js
@@ -0,0 +1,6 @@
+// export const PERSONAL_INFO = 'PERSONAL_INFO';
+// export const WALLET_INFO = 'WALLET_INFO';
+
+// export const requestAPI = () => ({ type: REQUEST_API });
+
+// export const getResultApi = (data) => ({ type: SUCCES_API, data });
---
+++ src/redux/reducers/player.js
@@ -0,0 +1,16 @@
+// import { PERSONAL_INFO } from '../actions';
+
+const initialState = {
+  name: '',
+  assertions: 0,
+  score: 0,
+  gravatarEmail: '',
+};
+
+const player = (state = initialState, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+export default player;
