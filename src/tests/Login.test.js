e9a7ebca90501676c5beee27e01834c8a27bf828
Thomas Boos Pegler <tboospegler@gmail.com>
2022-09-08 15:43:49 -0400

    Correção lint

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,32 +1,29 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
+import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
 import App from '../App';
 
 describe('tests made in the Login page', () => {
   it('should have a input for player name', () => {
-
     renderWithRouterAndRedux(<App />);
     const playerNameInput = screen.getByTestId('input-player-name');
     expect(playerNameInput).toBeInTheDocument();
-
   });
-  it('should have a input for the user email', () => {
 
+  it('should have a input for the user email', () => {
     renderWithRouterAndRedux(<App />);
     const userEmailInput = screen.getByTestId('input-gravatar-email');
     expect(userEmailInput).toBeInTheDocument();
-
   });
+
   it('should have a button for start the game, and be disabled at the start', () => {
-    
     renderWithRouterAndRedux(<App />);
     const playButton = screen.getByTestId('btn-play');
     expect(playButton).toBeInTheDocument();
     expect(playButton).toBeDisabled();
-
   });
+
   test('if the button activate once the form is filled', () => {
     renderWithRouterAndRedux(<App />);
     const playButton = screen.getByTestId('btn-play');
@@ -37,6 +34,9 @@ describe('tests made in the Login page', () => {
     expect(playButton).toBeEnabled();
   });
 
+  it('', () => {
+
+  });
 });
 
 // describe('', () => {
