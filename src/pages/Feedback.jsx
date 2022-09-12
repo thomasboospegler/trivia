2abe733e3ca6ad0d4171b53d223b1bc724adb0c7
Gabriel Puttin <gabrielputtin@hotmail.com>
2022-09-12 15:06:02 -0300

    Finalizando req 14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { number } from 'prop-types';
 
-export default class Feedback extends Component {
+class Feedback extends Component {
   render() {
+    const { score, assertions } = this.props;
     return (
-      <div data-testid="feedback-text">Feedback</div>
+      <div data-testid="feedback-text">
+        <span data-testid="feedback-total-score">{score}</span>
+        <span data-testid="feedback-total-question">{assertions}</span>
+      </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  score: state.player.score,
+  assertions: state.player.assertions,
+});
+
+Feedback.propTypes = {
+  score: number.isRequired,
+  assertions: number.isRequired,
+};
+
+export default connect(mapStateToProps)(Feedback);
