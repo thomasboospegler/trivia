4dbba35a3db1e13e5f40f4f2bcd88e3266a0587a
JoãoMarcos <104360924+JoaoMarcos0304@users.noreply.github.com>
2022-09-12 15:21:08 -0300

    Finalizando req 13

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -3,12 +3,25 @@ import { connect } from 'react-redux';
 import { number } from 'prop-types';
 
 class Feedback extends Component {
+  state = {
+    text: '',
+  };
+
+  componentDidMount() {
+    const { assertions } = this.props;
+    const MIN = 3;
+    if (assertions < MIN) return this.setState({ text: 'Could be better...' });
+    if (assertions >= MIN) return this.setState({ text: 'Well Done!' });
+  }
+
   render() {
     const { score, assertions } = this.props;
+    const { text } = this.state;
     return (
       <div data-testid="feedback-text">
         <span data-testid="feedback-total-score">{score}</span>
         <span data-testid="feedback-total-question">{assertions}</span>
+        <span data-testid="feedback-text">{ text }</span>
       </div>
     );
   }
